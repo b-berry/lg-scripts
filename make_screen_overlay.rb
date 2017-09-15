920b1459bb8614f0d2b18aed0e3d15ab43c1a61c
Bryan Berry <bryan@endpoint.com>
2017-09-15 14:49:18 -0700

    make_screen_overlay: Fixed imgname handling

--- make_screen_overlay.rb
+++ make_screen_overlay.rb
@@ -2,6 +2,7 @@
 # vim:ts=4:sw=4:et:smartindent:nowrap
 require 'erb'
 require 'fileutils'
+require 'pry'
 
 # Define Defaults
 $overlayXY = %w(0 fraction 1 fraction 0 fraction 1 fraction -1 fraction -1 fraction)
@@ -10,7 +11,7 @@ TemplateOverlayKML = %(<?xml version="1.0" encoding="UTF-8"?>
   <Document>
     <ScreenOverlay id="<%= name %>-id">
         <name><%= name %></name>
-        <Icon><href><%= name %><%= filetype %></href></Icon>
+        <Icon><href><%= filename %></href></Icon>
         <overlayXY x="<%= so_xy[0] %>" y="<%= so_xy[2] %>" xunits="<%= so_xy[1] %>" yunits="<%= so_xy[3] %>"/>
         <screenXY x="<%= so_xy[4] %>" y="<%= so_xy[6] %>" xunits="<%= so_xy[5] %>" yunits="<%= so_xy[7] %>"/>
         <rotationXY x="0" y="0" xunits="fraction" yunits="fraction"/>
@@ -31,7 +32,6 @@ def makeOverlayKML(dir)
         renderer = ERB.new TemplateOverlayKML
         filename = File.basename(i)
         filetype = File.extname(filename)
-        #name = filename.gsub(' ','-').downcase
         name = File.basename(i,File.extname(i)).gsub(' ','-').downcase
         so_xy = $overlayXY
         kml_file = "#{dir}/#{name}.kml"
